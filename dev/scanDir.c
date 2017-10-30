ceeaec3e4acb71b4d1e17a01a74e165e46cdb463
Timofei Larkin <lllamnyp@gmail.com>
2017-10-30 16:03:36 +0100

    Add files via upload

--- dev/scanDir.c
+++ dev/scanDir.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <stdbool.h>
 #include "parseBig.h"
 
 char * get_extension(char * s)
@@ -18,31 +19,52 @@ char * get_extension(char * s)
 int main(int argc, char * argv[])
 {
 	struct BIGHeader hp;
+	char outname[250];
 	char fullname[250];
 	FILE * f;
     DIR *d;
+    bool hash_match = false;
     struct dirent *dir;
-    d = opendir(argv[1]);
+    
+    printf("Please wait while directory is being scanned...");
 
-    if (d)
-    {
-    	strcpy(fullname, argv[1]); // put the path into fullname
-    	char * name = fullname + strlen(fullname); // create a pointer to the end of that
-    	strcpy(name, "\\"); name++; // append a slash; increment the pointer
-    	
-		f = fopen("out", "w");
-        while ((dir = readdir(d)) != NULL)
-        {
-        	if (strcmp(get_extension(dir->d_name), ".big") == 0)
-        	{
-            	strcpy(name, dir->d_name);
-				parse_big(fullname, &hp);
-				put_big(&hp, f);
-			}
-        }
-        fclose(f);
-        closedir(d);
-    }
+	strcpy(outname, "./cache/");
+	strcpy(outname + 8, argv[2]); // outname now points to "./cache/[hash=argv[2]]"
+   	
+	f = fopen(outname, "r");
+	
+	if (f)
+	{
+		fscanf(f, "%[^\n]", fullname); // the first line of f should be the hash of that dir
+		if (strcmp(fullname, argv[3]) == 0) {hash_match = true;}
+		fclose(f);
+	}
 
+	if (!hash_match)
+	{
+	    d = opendir(argv[1]);
+	
+	    if (d)
+	    {
+			strcpy(fullname, argv[1]); // put the path into fullname
+			char * name = fullname + strlen(fullname); // create a pointer to the end of that
+			strcpy(name, "\\"); name++; // append a slash; increment the pointer
+			    	
+			f = fopen(outname, "w");
+			fputs(argv[3], f); fprintf(f, "\n");
+			
+		    while ((dir = readdir(d)) != NULL)
+		    {
+		    	if (strcmp(get_extension(dir->d_name), ".big") == 0)
+		    	{
+		        	strcpy(name, dir->d_name);
+					parse_big(fullname, &hp);
+					put_big(&hp, f);
+				}
+		    }
+		    fclose(f);
+		    closedir(d);
+		}
+	}
 	return 0;
 }
