3dec08133dea871ed3afc037a5278f9ef6dbe85e
Timofei Larkin <lllamnyp@gmail.com>
2017-10-30 16:04:30 +0100

    Working prototype of new directory browser

--- BIGParserAndMerger.m
+++ BIGParserAndMerger.m
@@ -167,9 +167,6 @@ ImportEPD[name_String]:=
 	]
 
 
-Unprotect[ImportBIG]
-
-
 ImportBIG[names_, p0_, a0_, depol_, four_] := ImportBIG[names, N@p0, N@a0, N@depol, four]
 ImportBIG[names_List, p0_Real, a0_Real, depol_, fourierFunc_]:=
 	With[
@@ -307,6 +304,22 @@ ImportVASE[name_String]:=
 
 
 Begin["`MergingInterface`"];
+
+
+$PackageDirectory = DirectoryName[$InputFileName];
+
+
+scanDir[dir_] := With[{currentDir = Directory[]},
+	SetDirectory[$PackageDirectory];
+	Run["scanDir.exe",#,IntegerString[Hash[#],36],IntegerString[Hash[FileNames["*.big",#]],36]]&[dir];
+	SetDirectory[currentDir];
+	With[{strm = OpenRead[FileNameJoin[{$PackageDirectory,"cache", IntegerString[Hash[dir],36]}]]},
+		ReadList[strm, String, 1];
+		Block[{out = ReadList[strm,{Word,Word,Number,Number,Number,Number,Number,Number,Number,Word},WordSeparators->{"\t"}]},
+			Close[strm];
+			out
+		]
+	]]
 
 
 mergingComplete=False;
@@ -702,6 +715,76 @@ BrowseFilesButton[idx_] :=
 	]
 
 
+SetAttributes[render, HoldAll];
+render[preview_, selected_] :=
+	Join[{MapThread[Function[{name,num},Button[name,preview=SortBy[preview,#[[num]]&]]],
+			{{"Name","Comment","T","AOI","POLA","Scans","Resolution","Min","Max","Range"},Range[10]}]},
+		Map[{Button[FileNameTake[#[[1]]],AppendTo[selected,#[[1]]]]}~Join~#[[2;;]]&, preview]] //
+	Grid[#, Background -> {{GrayLevel[1], None, GrayLevel[1]},{{GrayLevel[0.85], RGBColor[0.88, 1, 0.88]}}}, Frame -> All]&
+
+
+SetAttributes[remover,HoldFirst];
+remover[list_] := Column@MapIndexed[Button[#,list=Delete[list,First@#2]]&,list]
+
+
+preload[idx_]:=CreateDialog[
+
+With[{path = FileNameTake[
+				FirstCase[
+					{{Database[idx,"Files"]}, Table[Database[i, "Files"], {i, IRRanges + VISRanges}]},
+					_String, $UserDocumentsDirectory, {3}
+				],
+				{1,-2}]},
+With[{dirs=FileNameJoin[FileNameSplit[path][[;;#]]]&/@Range[Length@FileNameSplit[path]]},
+
+DynamicModule[
+	{preview={},selected={},init=Select[#<>":\\"&/@CharacterRange["A","Z"],DirectoryQ],directoryTree,directorySubTree},
+	Dynamic[
+		Panel[
+			Pane[
+				Grid[{{Pane[Column[directoryTree/@init],{350,Full},Scrollbars->True],
+					Pane[Dynamic[render[preview,selected]],{Full,800},Scrollbars->True],
+					Column[{Dynamic[remover@selected],DefaultButton[DialogReturn[Database[idx,"Files"] = selected]]}]}}, Alignment->{Left,Top}],
+				{Full,800},Scrollbars->{False,True},AppearanceElements->All,BaseStyle->LineBreakWithin->False
+			],
+			"Viewer"
+		],
+		UnsavedVariables :> {preview,directoryTree,directorySubTree},
+		Initialization :> {
+			selected={};
+			preview={};
+			directoryTree[root_String?DirectoryQ] :=
+				DynamicModule[{open=MemberQ[dirs,root],subTree=If[MemberQ[dirs,root],directorySubTree[root],""]},
+					PaneSelector[{
+						False -> #,
+						True -> Column[{#,
+							Dynamic[subTree]}]}, Dynamic[open],ImageSize->Automatic] & [
+						Row[{
+							Opener[Dynamic[open,{Automatic,If[#,subTree=directorySubTree[root],subTree=.]&}]],
+							MouseAppearance[EventHandler[
+								FileNameTake[root],
+								"MouseClicked":>(preview=scanDir[root]),Method->"Queued"],"LinkHand"]
+						}]
+					]
+				];
+			directorySubTree[root_String?DirectoryQ] :=
+				Module[{subDirs,offset},
+					subDirs = Quiet@Check[Select[FileNames["*",root],DirectoryQ],$Failed];
+					offset=Grid[{{Invisible@Opener[],#}},Spacings->{0,0}]&;
+					offset@Switch[subDirs,
+						$Failed,Row[{Style["\[WarningSign]",20]," access denied"},BaseStyle->Orange],
+						{},Style["\[FreakedSmiley]",Blue,20],
+						_,Column[directoryTree/@Select[FileNames["*",root],DirectoryQ]]]
+				]
+		}
+	]
+] ] ] ]
+
+
+BetterBrowseButton[idx_] :=
+	Button["Browse\nbetter...", preload[idx]];
+
+
 BrowseCompsButton[idx_/;idx<=IRRanges] :=
 	Button[
 		"Browse\n...",
@@ -775,7 +858,7 @@ FileSelect[] :=
 						{
 							Style[i,16,GrayLevel[.75],FontFamily->"Consolas"],
 							ScrollBox[Database[i, "Files"]//StringRiffle[#,"\n"]&,{30,5}],
-							BrowseFilesButton[i],
+							Column[{BrowseFilesButton[i],BetterBrowseButton[i]}],
 							CompSelector[i],
 							ScrollBox[Database[i, "Comp", "Files"]//StringRiffle[#,"\n"]&,{30,5}],
 							Column[{BrowseCompsButton[i],ClearCompsButton[i]}]}],
